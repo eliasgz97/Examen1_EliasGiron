714df4088b8cc3356992aef1e13b8ee0dadc604b
eliasgz97 <47019721+eliasgz97@users.noreply.github.com>
2019-02-15 18:28:43 -0600

    Add files via upload

---
+++ Examenlab1_EliasGiron/src/examenlab1_eliasgiron/Libros.java
@@ -0,0 +1,107 @@
+package examenlab1_eliasgiron;
+
+import java.util.Date;
+
+public class Libros {
+    private String titulo;
+    private String descripcion;
+    private int puntaje;
+    private int copias;
+    private String genero;
+    private int valor;
+    private String edicion, autor;
+    private Date publicacion;
+
+    public Libros() {
+    }
+
+    public Libros(String titulo, String descripcion, int puntaje, int copias, String genero, int valor, String edicion, String autor, Date publicacion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.puntaje = puntaje;
+        this.copias = copias;
+        this.genero = genero;
+        this.valor = valor;
+        this.edicion = edicion;
+        this.autor = autor;
+        this.publicacion = publicacion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public int getCopias() {
+        return copias;
+    }
+
+    public void setCopias(int copias) {
+        this.copias = copias;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(String edicion) {
+        this.edicion = edicion;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Date getPublicacion() {
+        return publicacion;
+    }
+
+    public void setPublicacion(Date publicacion) {
+        this.publicacion = publicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Libros{" + "titulo=" + titulo + '}';
+    }
+    
+}
